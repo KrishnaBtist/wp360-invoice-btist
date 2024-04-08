418479a9846b8c5277f2fc59a354403a6b492793
Krishna Btist <75871269+KrishnaBtist@users.noreply.github.com>
2024-04-08 11:41:58 +0530

    Update wp360-invoice.php

--- wp360-invoice.php
+++ wp360-invoice.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: Wp360 Invoice
-  Description: The WP360 Invoice Plugin provides an intuitive solution to manage and create invoices seamlessly for woocommerce websites. 
+  Description: The WP360 Invoice Plugin provides an intuitive solution to manage and create invoices seamlessly for woocommerce websites checking everywhere. 
   Requires at least: WP 5.2.0
   License:GPL2
   Tested up to: WP 6.5.6
